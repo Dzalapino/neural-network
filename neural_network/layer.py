ae29a234d40956f16a164bd7201d9d6e013ac97d
Kuba Podkomórka <jpodkomorka19@gmail.com>
2023-11-28 13:36:13 +0100

    Added pandas and iris dataset to the project. Imported dataset in main

---
+++ neural_network/layer.py
@@ -0,0 +1,5 @@
+
+
+class Layer:
+    def __init__(self, n_inputs, n_neurons):
+        pass
--- neural_network/main.py
+++ neural_network/main.py
@@ -1,7 +1,11 @@
 
 
 def main():
-    print("Hello from main!")
+    import pandas as pd
+
+    # Import the iris dataset
+    iris_df = pd.read_csv('resources/iris.csv', index_col=0)
+    print(iris_df)
 
 
 if __name__ == '__main__':
