d51cfe55730323d267548be34f4cb525bcedfc6e
Kuba Podkomórka <jpodkomorka19@gmail.com>
2023-11-28 23:49:36 +0100

    Added the __str__() override to neural network module, initializing train and eval X and y objects. Added neural network to main

--- main.py
+++ main.py
@@ -2,11 +2,15 @@
 
 def main():
     from neural_network.dataset import Dataset
+    import neural_network.activation_functions as activation_functions
+    from neural_network.neural_network import NeuralNetwork
 
     dataset = Dataset('resources/Iris.csv', 0.8)
-    print(dataset.df)
-    print(dataset.train_df)
-    print(dataset.eval_df)
+
+    model = NeuralNetwork(dataset.train_X, dataset.train_y, dataset.eval_X, dataset.eval_y)
+    model.add_layer(4, 10, activation_functions.relu)
+    model.add_layer(10, 8, activation_functions.relu)
+    model.add_layer(8, 3, activation_functions.softmax)
 
 
 if __name__ == '__main__':
--- neural_network/neural_network.py
+++ neural_network/neural_network.py
@@ -7,8 +7,19 @@ from neural_network.layer import Layer
 
 
 class NeuralNetwork:
-    def __init__(self, x_train, x_eval, y_train, y_eval):
+    def __init__(self, train_x, train_y, eval_x, eval_y):
+        self.train_X = train_x
+        self.train_y = train_y
+        self.eval_X = eval_x
+        self.eval_y = eval_y
         self.layers: list[Layer] = []
+
+    def __str__(self):
+        n_layers = len(self.layers)
+        s = f'Neural network model.\nNumber of layers: {n_layers}'
+        for i in range(n_layers):
+            s += f'\nNumber of neurons in the {i+1} layer: {np.size(self.layers[i].outputs, 1)}'
+        return s
 
     def add_layer(self, n_inputs: int, n_neurons: int, activation_function: Callable[[np.ndarray], np.ndarray]) -> None:
         self.layers.append(Layer(n_inputs, n_neurons, activation_function))
