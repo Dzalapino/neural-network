37fe4f09a011f5a87504ac563beff8eb7f8ae972
Kuba Podkomórka <jpodkomorka19@gmail.com>
2023-11-28 23:46:53 +0100

    Added the __str__() override to layer module, changed the weights initialization to He method

--- neural_network/layer.py
+++ neural_network/layer.py
@@ -6,10 +6,17 @@ class Layer:
     def __init__(self, n_inputs, n_neurons,
                  activation_function: Callable[[np.ndarray], np.ndarray]
                  ):
-        self.weights: np.ndarray = np.random.randn(n_inputs, n_neurons)  # Init weights
-        self.biases: np.ndarray = np.zeros((1, n_neurons))  # Init biases
-        self.outputs: np.ndarray = np.empty((1, n_neurons))  # Init outputs
-        self.activation_function: Callable[[np.ndarray], np.ndarray] = activation_function  # Pass activation function
+        # Init weights using He initialization. Normal(mean, standard_deviation)
+        self.weights: np.ndarray = np.random.normal(0, np.sqrt(2 / n_inputs), (n_inputs, n_neurons))
+        # Init biases and outputs
+        self.biases: np.ndarray = np.zeros((1, n_neurons))
+        self.outputs: np.ndarray = np.empty((1, n_neurons))
+        # Pass activation function
+        self.activation_function: Callable[[np.ndarray], np.ndarray] = activation_function
+
+    def __str__(self):
+        return (f"Layer:\nweights: {self.weights}\nbiases: {self.biases}\n"
+                f"outputs: {self.outputs}\nactivation function: {self.activation_function}")
 
     def forward_pass(self, inputs) -> None:
         # Calculate the outputs and pass them to the activation function
